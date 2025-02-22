feec3ae9245bc56c5f20e3c3d1c8e11251394865
mk7193 <mk7193@thryv.com>
2025-02-21 18:59:42 -0700

    Initial Commit

---
+++ Metar Map Plus/Metar_Map_PlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metar_Map_PlusApp.swift
+//  Metar Map Plus
+//
+//  Created by Kuriger, Michael on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Metar_Map_PlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Metar Map PlusTests/Metar_Map_PlusTests.swift
@@ -0,0 +1,16 @@
+//
+//  Metar_Map_PlusTests.swift
+//  Metar Map PlusTests
+//
+//  Created by Kuriger, Michael on 2/21/25.
+//
+
+import Testing
+
+struct Metar_Map_PlusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
